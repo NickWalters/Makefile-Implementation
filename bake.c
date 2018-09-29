e4d6d24c0b29a95e26b309ee0a0b77bc54e971bb
Nick Walters <nicho.walters@gmail.com>
2018-09-29 14:38:13 +0800

    .
    .

--- bake.c
+++ bake.c
@@ -47,8 +47,8 @@ int main(int argc, char *argv[])
                 commentNum++;
             }
         }
-        printf("\n");
-        printf("The number of comments is: %i\n", commentNum); // this is for testing purposes
+        printf("\n\n");
+        printf("---------The number of comments is: %i ----------\n", commentNum); // this is for testing purposes
         
         // if the line has a " = " in it, then consider the line to be a variable declaration.
         
