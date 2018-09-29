cafb3067e6a5a0dd3f0a51b1ab697031a2f9e3b6
Nick Walters <nicho.walters@gmail.com>
2018-09-29 14:22:18 +0800

    .
    .s

--- bake.c
+++ bake.c
@@ -14,40 +14,6 @@
 #include <unistd.h>
 
 
-void findwords(FILE *fp)
-{
-    char   line[BUFSIZ];
-    
-    //  READ EACH LINE OF THE FILE
-    while(fgets(line, sizeof line, fp) != NULL) {
-        char    *ch = line;
-        
-        //  CONSIDER EACH CHARACTER OF THE LINE, LOOKING FOR 'WORDS'
-        while(*ch != '\0') {
-            char   word[BUFSIZ];
-            char   *w  = word;
-            *w = *w +1;
-            // if the line has a " = " in it, then consider the line to be a variable declaration.
-            while(isalpha(*ch)) {
-                // scan each character of the line. check if its alphanumeric/letter
-                *w++ = *ch++;
-            }
-            *w  = '\0';
-            
-            if(w != word) {
-                //addToList(word);
-            }
-            ++ch;
-            
-            // if the line has a " target: " in it, then treat dependancies
-            
-            
-            
-            // the line after the target line is an action.
-        }
-    }
-}
-
 
 
 bool comment(char * ch)
@@ -70,8 +36,21 @@ int main(int argc, char *argv[])
 {
     //  ATTEMPT TO OPEN AND READ FROM PROVIDED FILENAME
     if(fopen("Bakefile.txt", "r") != NULL) {
-        FILE *fp = fopen("Bakefile", "r");
-        findwords(fp);
+        FILE *fp = fopen("Bakefile.txt", "r");
+        
+        char   line[BUFSIZ];
+        //  READ EACH LINE OF THE FILE
+        while(fgets(line, sizeof line, fp) != NULL){
+            printf("%s",line);
+        }
+        // if the line has a " = " in it, then consider the line to be a variable declaration.
+        
+        
+        // if the line has a " target: " in it, then treat dependancies
+        
+        
+        
+        // the line after the target line is an action.
         fclose(fp);
     }
     //  NOT PRESENT IN DIRECTORY, SO TRY PRESENT WORKING DIRECTORY
