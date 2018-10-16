aa002009f7386ebb1a1483f6864d959b19573701
Nick Walters <nicho.walters@gmail.com>
2018-10-16 21:14:59 +0800

    .
    .

---
+++ bake.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include  <sys/types.h>
+#include  <sys/stat.h>
+#include  <sys/param.h>
+#include  <dirent.h>
+#include  <time.h>
+#include <unistd.h>
+#include <limits.h>
+
+void targetLines(void);
--- target.c
+++ target.c
@@ -176,12 +176,14 @@ void rebuildCheck(){
             }
             else{
                 if(files[i][j] != NULL){
-                    char * path = pathAppend(files[i][j]);
-                    time_t dependancyCreationDate = getFileCreationTime(path);
-                    if(difftime(dependancyCreationDate, targetCreationDate) > 0){
-                        rebuildFile[rebuildCount] = files[i][j];
-                        printf("rebuildFile[%i]: %s\n", rebuildCount, rebuildFile[rebuildCount]);
-                        ++rebuildCount;
+                    if(files[i][j] != NULL){
+                        char * path = pathAppend(files[i][j]);
+                        time_t dependancyCreationDate = getFileCreationTime(path);
+                        if(difftime(dependancyCreationDate, targetCreationDate) > 0){
+                            rebuildFile[rebuildCount] = files[i][j];
+                            printf("rebuildFile[%i]: %s\n", rebuildCount, rebuildFile[rebuildCount]);
+                            ++rebuildCount;
+                    }
                 }
                 else{
                     continue;
